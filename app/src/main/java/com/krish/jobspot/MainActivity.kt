1fd1c18cddf02b4e10ef4c4d29f4683003521fe9
Krish Parekh <Krishp4123@gmail.com>
2022-08-15 20:00:09 +0530

    JobSpot - Implemented SplashScreen for both the theme and added essential colors for app

--- app/src/main/java/com/krish/jobspot/MainActivity.kt
+++ app/src/main/java/com/krish/jobspot/MainActivity.kt
@@ -2,9 +2,13 @@ package com.krish.jobspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
