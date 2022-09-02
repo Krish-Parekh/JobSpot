6a3cfcd722f86c01ee89e0c2a38786027062989c
Krish Parekh <Krishp4123@gmail.com>
2022-09-02 11:09:46 +0530

    JobSpot - Implemented Material DatePicker and used 3rd party library for ImageView Picker for resize and crop also enabled Jetifier in settings.gradle

--- app/src/main/java/com/krish/jobspot/user_details/fragments/StudentDetailFragment.kt
+++ app/src/main/java/com/krish/jobspot/user_details/fragments/StudentDetailFragment.kt
@@ -1,23 +1,88 @@
 package com.krish.jobspot.user_details.fragments
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.krish.jobspot.R
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.krish.jobspot.databinding.FragmentStudentDetailBinding
-
 
 class StudentDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentStudentDetailBinding
+    private lateinit var imageUri: Uri
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            handleCapturedImage(result)
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentStudentDetailBinding.inflate(inflater, container, false)
+        setupView()
         return binding.root
+    }
+
+    private fun setupView() {
+        binding.etDate.isCursorVisible = false
+        binding.etDate.keyListener = null
+        binding.etDateContainer.setEndIconOnClickListener {
+            showCalendar()
+        }
+
+        binding.profileImage.setOnClickListener {
+            startCrop()
+        }
+    }
+
+    private fun startCrop() {
+        ImagePicker.with(this)
+            .galleryOnly()
+            .crop()
+            .compress(1024)
+            .maxResultSize(300, 300)
+            .createIntent { intent ->
+                startForProfileImageResult.launch(intent)
+            }
+    }
+
+    private fun handleCapturedImage(result: ActivityResult) {
+        val resultCode = result.resultCode
+        val data = result.data
+
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                imageUri = data?.data!!
+                binding.profileImage.setImageURI(imageUri)
+            }
+            ImagePicker.RESULT_ERROR -> {
+                Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT)
+                    .show()
+            }
+            else -> {
+                Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showCalendar() {
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+        datePicker.addOnPositiveButtonClickListener {
+            binding.etDate.setText(datePicker.headerText)
+        }
+        datePicker.show(childFragmentManager, "Material DatePicker")
     }
 }
